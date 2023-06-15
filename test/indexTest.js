38706756cc74c767bf0a9b64c3c1ab2bcda9b52b
John mk UBUNTU <jmakateto@gmail.com>
2023-06-15 22:16:55 +0300

    Completed assignment

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,30 +1,40 @@
-require("./helpers.js");
+const { expect } = require("chai");
+const fs = require("fs");
 
 describe("within index.html", () => {
+  let html;
+
+  before(() => {
+    html = fs.readFileSync("./index.html", "utf-8");
+  });
+
   it("contains a <body> tag", () => {
-    expect(document.querySelector("html").innerHTML).to.include("<body>");
+    expect(html).to.include("<body>");
   });
+
   it("contains a </body> tag", () => {
-    expect(document.querySelector("html").innerHTML).to.include("</body>");
+    expect(html).to.include("</body>");
   });
+
   it("contains an <h1> tag", () => {
-    expect(document.querySelector("body").innerHTML).to.include("<h1>");
+    expect(html).to.include("<h1>");
   });
+
   it("contains a <p> tag", () => {
-    expect(document.querySelector("body").innerHTML).to.include("<p>");
+    expect(html).to.include("<p>");
   });
+
   it("within the <p>, it contains a <strong> tag", () => {
-    expect(document.querySelector("p").innerHTML).to.include("<strong>");
+    const pTag = html.match(/<p>(.*?)<\/p>/s)[1];
+    expect(pTag).to.include("<strong>");
   });
+
   it("within the <p>, it contains an <em> tag", () => {
-    expect(document.querySelector("p").innerHTML).to.include("<em>");
+    const pTag = html.match(/<p>(.*?)<\/p>/s)[1];
+    expect(pTag).to.include("<em>");
   });
-  it("within the <p>, it contains an <a> tag", () => {
-    expect(document.querySelector("p").innerHTML).to.include(
-      '<a href="https://developer.mozilla.org/en-US/docs/Web/HTML">'
-    );
-  });
+
   it("within the <body>, it contains a <table> tag", () => {
-    expect(document.querySelector("body").innerHTML).to.include("table");
+    expect(html).to.include("<table>");
   });
 });
